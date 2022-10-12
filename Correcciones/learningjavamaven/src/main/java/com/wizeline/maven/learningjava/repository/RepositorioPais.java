f44e6c385b6555bcc44b0c96f5e8c0c5e90f1b83
JUAN CARLOS LOPEZ ESTRADA <jclestrada58@gmail.com>
2022-10-12 10:36:04 -0500

    basura

--- Correcciones/learningjavamaven/src/main/java/com/wizeline/maven/learningjava/repository/RepositorioPais.java
+++
@@ -1,8 +0,0 @@
-package com.wizeline.maven.learningjava.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.wizeline.gradle.learningjava.model.PaisesDTO;
-
-public interface RepositorioPais extends MongoRepository<PaisesDTO,String> {
-}
